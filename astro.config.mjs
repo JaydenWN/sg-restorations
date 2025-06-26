036977d74888b6e66bc23f385562bcb548daf1ba
JaydenWN <123134488+JaydenWN@users.noreply.github.com>
2025-06-26 17:11:21 +1000

    changed build output settings, added company url

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,8 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://sg-restorations.vercel.app/",
+	site: "https://sgrestorations.com.au",
+		output: "static",
 	integrations: [mdx(), sitemap()],
 	vite: {
 		resolve: {
