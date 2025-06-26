2cea9276ed2772ea429b2b72606584c73507ee03
JaydenWN <123134488+JaydenWN@users.noreply.github.com>
2025-06-26 15:11:14 +1000

    responsive site submission 1

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'SG Restorations | Mould and Moisture Damage Specialists';
+export const SITE_DESCRIPTION = 'SG Restorations is a family-owned business providing certified mould removal, moisture assessments and deep cleaning services across the Central Coast, Newcastle and Sydney. We help homeowners, buyers, renters and property managers tackle mould and moisture issues with reliable, professional care. From detailed inspections to full remediation and air quality testing, we restore homes the right way – with honesty, safety and a hands-on local approach.';
